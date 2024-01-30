12236566ff91907be61cdecc5eebeb317ad19436
Tarun Chawla <65712331+Tarun193@users.noreply.github.com>
2024-01-30 16:39:57 -0500

    created basic loginActivity and connected with firebase

--- app/src/main/java/com/example/birthdaywhisher/MainActivity.kt
+++ app/src/main/java/com/example/birthdaywhisher/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.birthdaywhisher
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.birthdaywhisher.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater);
+        val db = Firebase.firestore
+        db.collection("Users").get().addOnSuccessListener {
+            result -> for(document in result){
+                Log.d("Firebase", "${document.id} => ${document.data}");
+        }
+        }.addOnFailureListener {
+                exception ->
+            Log.w("Firebase", "Error getting documents.", exception)
+        }
+        setContentView(binding.root);
+
+        binding.button.setOnClickListener{
+            var email = binding.editEmail.text.toString();
+            var password = binding.editPassword.text.toString();
+
+            Log.i("Test", "$email $password");
+        }
+
     }
 }
